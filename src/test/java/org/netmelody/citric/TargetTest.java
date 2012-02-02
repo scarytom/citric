563350a2ed47d62ce774b5c8a9cdd2704100d4aa
scarytom <t.denley@cantab.net>
2012-02-02 20:53:44 +0000

    with less feeling

--- src/test/java/org/netmelody/citric/TargetTest.java
+++ src/test/java/org/netmelody/citric/TargetTest.java
@@ -3,17 +3,12 @@ package org.netmelody.citric;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import java.util.Arrays;
-
 import org.hamcrest.Matchers;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.UnmodifiableListIterator;
 
 public final class TargetTest {
 
@@ -62,13 +57,6 @@ public final class TargetTest {
     @Test public void
     realisticallySimulatesCommitBuilds() {
         final Target target = new Target(new CommitStream(), Time.of(2));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(1)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(2)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(3)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(4)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(5)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(6)), Artefact.class)));
-        System.out.println(Arrays.deepToString(Iterables.toArray(target.availableAt(Time.of(7)), Artefact.class)));
         assertThat(target.availableAt(Time.of(0)), is(Matchers.<Artefact>emptyIterable()));
         assertThat(target.availableAt(Time.of(1)), is(Matchers.<Artefact>emptyIterable()));
         assertThat(target.availableAt(Time.of(2)), is(Matchers.<Artefact>emptyIterable()));
