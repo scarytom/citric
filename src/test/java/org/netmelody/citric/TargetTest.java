c77f1ceb1d4230ca3a03ac9b9def5b2d1b1ef75d
scarytom <t.denley@cantab.net>
2012-02-03 13:31:40 +0000

    testcase for imminent artifacts from Target

--- src/test/java/org/netmelody/citric/TargetTest.java
+++ src/test/java/org/netmelody/citric/TargetTest.java
@@ -2,6 +2,7 @@ package org.netmelody.citric;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 import org.hamcrest.Matchers;
 import org.jmock.Expectations;
@@ -52,6 +53,25 @@ public final class TargetTest {
         
         assertThat(target.availableAt(Time.of(5)), is(Matchers.<Artefact>emptyIterable()));
         assertThat(target.availableAt(Time.of(6)), is(Matchers.contains(Artefact.number(1))));
+    }
+    
+    @Test public void
+    imminentlySuppliesNextArtifact() {
+        final ArtefactStream stream = context.mock(ArtefactStream.class);
+        final Target target = new Target(stream, Time.of(5));
+        
+        context.checking(new Expectations() {{
+            allowing(stream).availableAt(Time.of(1)); will(returnValue(ImmutableSortedSet.of(Artefact.number(1))));
+            allowing(stream).availableAt(Time.of(6)); will(returnValue(ImmutableSortedSet.of(Artefact.number(1), Artefact.number(2))));
+        }});
+        
+        assertThat(target.imminentAt(Time.of(0)).orNull(), is(nullValue()));
+        assertThat(target.imminentAt(Time.of(1)).orNull(), is(Artefact.number(1)));
+        assertThat(target.imminentAt(Time.of(2)).orNull(), is(Artefact.number(1)));
+        assertThat(target.imminentAt(Time.of(3)).orNull(), is(Artefact.number(1)));
+        assertThat(target.imminentAt(Time.of(4)).orNull(), is(Artefact.number(1)));
+        assertThat(target.imminentAt(Time.of(5)).orNull(), is(Artefact.number(1)));
+        assertThat(target.imminentAt(Time.of(6)).orNull(), is(Artefact.number(2)));
     }
     
     @Test public void
